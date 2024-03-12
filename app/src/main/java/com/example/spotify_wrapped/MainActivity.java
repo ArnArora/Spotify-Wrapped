bda9c812a1504cfd7a50a15ed447b56ba862b969
arushigupta3037 <agupta3037@gatech.edu>
2024-03-12 15:06:16 -0400

    Added Functionality to Base User Story One and update UI to include background.

--- app/src/main/java/com/example/spotify_wrapped/MainActivity.java
+++ app/src/main/java/com/example/spotify_wrapped/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.spotify_wrapped;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        Button loginbutton = (MaterialButton) findViewById(R.id.loginButton);
+        //admin and admin
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "LOGIN NOT SUCCESSFUL", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
+
+
 }
